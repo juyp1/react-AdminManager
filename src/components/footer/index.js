dce78b365271ee5084c441711804adee5a2de734
juyp1 <76542009@qq.com>
2018-09-02 17:50:07 +0800

    add

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './index.less';
 class Footer extends Component {
   render(){
     return(
-      <div>
-        footer
+      <div className='footer'>
+         版权所有:一直努力思考的鱼🐟 感谢您使用通用管理系统(建议使用谷歌浏览器体验更佳) 2018~2020
       </div>
     )
   }
